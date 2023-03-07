740c082b1f85bc5a612527cb9d7aae4bc4377c84
gongzhangxun-ola <gong.zhangxun@olaola.chat>
2023-03-07 10:19:07 +0800

    add

--- c.go
+++ c.go
@@ -1,9 +1,13 @@
-package gxb
+package gxc
 
 import (
 	"log"
+
+	"github.com/gongzhxu/gx-b"
 )
 
 func CFunc() {
-	log.Println("this is c")
+	log.Println("this gx-c")
+
+	gxb.BFunc()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gongzhxu/gx-c
 
 go 1.18
+
+require github.com/gongzhxu/gx-b v0.1.1
